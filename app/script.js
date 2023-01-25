76c87d760856c92b21363734416c669b678035b3
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-01-25 10:14:09 +0100

    made changes to the mobile menu bug

--- app/script.js
+++ app/script.js
@@ -4,7 +4,7 @@ const menubar = document.querySelector(".menubar");
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const list = document.querySelectorAll("ul.menudrop");
-const menubtn = document.querySelectorAll(".menubtn");
+const menubtn = document.querySelectorAll("div.menubtn");
 
 menubar.addEventListener("click", () => {
   left.classList.toggle("open");
@@ -12,19 +12,22 @@ menubar.addEventListener("click", () => {
   menubar.classList.toggle("toggle");
 });
 
-// menubtn.forEach((navEl, i) => {
-//   navEl.addEventListener("click", () => {
-//     hideEl();
-//     list[i].classList.toggle("hidden");
-//   });
-// });
+menubtn.forEach((navEl, i) => {
+  navEl.addEventListener("click", () => {
+    hideEl();
+    navEl.classList.toggle("activeNav");
+    console.log(list[i]);
+    list[i].classList.toggle("hidden");
+  });
+});
 
-// document.addEventListener("click", (e) => {
-//   if (e.target.classList.contains("hidden")) hideEl();
-// });
 
 const hideEl = () => {
   list.forEach((item) => {
     if (!item.classList.contains("hidden")) item.classList.add("hidden");
   });
 };
+
+// document.addEventListener("click", (e) => {
+//   if (e.target.classList.contains("hidden")) hideEl();
+// });
