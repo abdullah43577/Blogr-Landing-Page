694059eebc3b98014ca986bd002b9f12153cbd09
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2023-01-25 17:50:52 +0100

    refactoring code

--- app/script.js
+++ app/script.js
@@ -3,48 +3,9 @@
 const menubar = document.querySelector(".menubar");
 const left = document.querySelector(".left");
 const right = document.querySelector(".right");
-const list = document.querySelectorAll("ul.menudrop");
-const menubtn = document.querySelectorAll("div.menubtn");
-const menu = document.querySelectorAll(".menu");
 
 menubar.addEventListener("click", () => {
   left.classList.toggle("open");
   right.classList.toggle("open");
   menubar.classList.toggle("toggle");
 });
-
-menubtn.forEach((navEl, i) => {
-  navEl.addEventListener("click", () => {
-    hideEl();
-    navEl.classList.toggle("activeNav");
-    list[i].classList.toggle("hidden");
-
-    menu.forEach((menuEl) => {
-      menuEl.classList.remove("el");
-    });
-    hideNav();
-  });
-});
-
-const hideEl = () => {
-  list.forEach((item) => {
-    if (!item.classList.contains("hidden")) item.classList.add("hidden");
-  });
-};
-
-const hideNav = () => {
-  document.addEventListener("click", (e) => {
-    if (
-      e.target.classList.contains("left") ||
-      e.target.classList.contains("menu") ||
-      e.target.classList.contains("menudrop") ||
-      e.target.textContent === "Product" ||
-      e.target.textContent === "Company" ||
-      e.target.textContent === "Connect"
-    ) {
-      console.log("valid");
-    } else {
-      hideEl();
-    }
-  });
-};
